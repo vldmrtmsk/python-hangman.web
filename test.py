99e955eee523276086c858973c045ed06c10b0fa
vldmrtmsk <vldmrtmsk@gmail.com>
2018-01-24 11:33:12 +0200

    test
    Test files project;)

---
+++ test.py
@@ -0,0 +1,10 @@
+#!C:/Language programing/python3/python.exe
+
+## Даєм заголовок html
+print("Content-type: text/html")
+## Відділяєм заголовок від тіла
+print()
+## Пишем hello world
+print("Hello, World!")
+
+## Тут був тест чи локальна машина зупустить пітон
